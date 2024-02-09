8ff0e41c50e85f7ad9b17396c4f8fe5808d282f4
Admas Kinfu <supersoftwarequalityassurance@gmail.com>
2024-02-09 03:49:38 -0800

    wip

---
+++ build_image_and_run_containers.sh
@@ -0,0 +1,17 @@
+set -x
+set -e
+
+export IMAGE_NAME=supersqa_testlink
+export VOLUME_DIR_TESTLINK=$(pwd)/volume_testlink
+export VOLUME_DIR_MYSQL=$(pwd)/volume_mysql
+
+
+echo "Building the image ......"
+docker build -t $IMAGE_NAME .
+
+
+echo "Running containers using docker-compose ...."
+docker-compose up -d
+
+# display container info
+docker ps
